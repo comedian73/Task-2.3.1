4a241071f659d50556c839e1930035f732e607f0
comedian73 <118024172+comedian73@users.noreply.github.com>
2023-07-09 01:35:45 +0300

    1

---
+++ src/main/java/hiber/model/User.java
@@ -0,0 +1,79 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(name = "name")
+   private String firstName;
+
+   @Column(name = "last_name")
+   private String lastName;
+
+   @Column(name = "email")
+   private String email;
+
+   @OneToOne(
+        mappedBy = "user",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true,
+        fetch = FetchType.LAZY
+   )
+   @JoinColumn(name = "c_id", referencedColumnName = "c_id")
+   public Car car;
+
+   public User() {}
+
+   public User(String firstName, String lastName, String email) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public void setCar(Car car) {
+      car.setUser( this );
+      this.car = car;
+   }
+
+   public void getCar(Car car) {
+      this.car = car;
+   }
+}
---
+++ src/main/java/hiber/service/CarService.java
@@ -0,0 +1,16 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface CarService {
+    void add(Car car);
+
+    List<Car> listCar();
+
+    void dropCarTable();
+
+    List<User> getUser(String model, int series);
+}
